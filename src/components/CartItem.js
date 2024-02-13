894b644a2bae93e8a362f13b2b0a9e8c7e6b55e1
Amit Kumar <15000amitkumar@gmail.com>
2024-02-13 22:11:13 +0530

    Cart Page

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { MdDeleteForever } from "react-icons/md";
 
-function CartItem() {
-  return <div>CartItem</div>;
+function CartItem({ item }) {
+  return (
+    <div>
+      <div>
+        <img src={item.image} alt="Item" />
+      </div>
+      <div>
+        <h1>{item.title}</h1>
+        <h1>{item.description}</h1>
+      </div>
+      <div>
+        <p>{item.price}</p>
+        <p>
+          <MdDeleteForever />
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default CartItem;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
+import CartItem from "../components/CartItem";
 
 function Cart() {
-  return <div>Cart</div>;
+  const { cart } = useSelector((state) => state);
+  const [totalAmount, setTotalAmount] = useState(0);
+
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
+  return (
+    <>
+      {cart.length > 0 ? (
+        <div>
+          <div>
+            {cart.map((item, index) => (
+              <CartItem key={item.id} item={item} itemIndex={index} />
+            ))}
+          </div>
+          <div>
+            <div>Your Cart</div>
+            <div>Your Summary</div>
+            <p>
+              <span>Total Items: {cart.length}</span>
+            </p>
+          </div>
+          <div>
+            <p>Total Amount: ${totalAmount}</p>
+            <button>CheckOut Now</button>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h1>Cart Empty</h1>
+          <NavLink to={"/"}>
+            <button>Shop Now</button>
+          </NavLink>
+        </div>
+      )}
+    </>
+  );
 }
 
 export default Cart;
