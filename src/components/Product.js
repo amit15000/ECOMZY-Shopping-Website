8d76761ff58fb76f8eeaaf7540ba8bb9cc1e6135
Amit Kumar <15000amitkumar@gmail.com>
2024-02-13 15:08:57 +0530

    Product Card

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Product() {
-  return <div>Product</div>;
+function Product({ card }) {
+  const [seleted, setSelected] = useState(false);
+  return (
+    <div>
+      <div>
+        <p>{card.title}</p>
+      </div>
+      <div>
+        <p>{card.description}</p>
+      </div>
+      <div>
+        <img src={card.image} alt="Product" />
+      </div>
+      <div>
+        <p>{card.price}</p>
+      </div>
+      {seleted ? <p>Remove Item</p> : <p>Add to Cart</p>}
+    </div>
+  );
 }
 
 export default Product;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "../components/Spinner";
+import Product from "../components/Product";
 
 function Home() {
   const API_URL = process.env.REACT_APP_API_URL;
@@ -20,7 +22,17 @@ function Home() {
     fetchProductsData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  return <div>Home</div>;
+  return (
+    <div>
+      {loading ? (
+        <Spinner />
+      ) : cards.length > 0 ? (
+        cards.map((card) => <Product key={card.id} card={card} />)
+      ) : (
+        <p>No card found</p>
+      )}
+    </div>
+  );
 }
 
 export default Home;
